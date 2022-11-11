3d11235964e2a34e0f271628068e10c4f3376031
Tvisha Thakur <yashi.thakur1@gmail.com>
2022-11-10 23:37:36 -0500

    Added search API

--- FinalProject/model/track.js
+++ FinalProject/model/track.js
@@ -1,12 +1,11 @@
-export default class Track {
-    constructor(track_id, name, view_url, price, release_date, image, artist_id, collection_id) {
+export default class Track{
+    constructor(track_id, name, url, price, release_date, fk_artist_id, fk_collection_id) {
         this.track_id = track_id;
         this.name = name;
-        this.view_url = view_url;
+        this.url = url;
         this.price = price;
         this.release_date = release_date;
-        this.image = image;
-        this.artist_id = artist_id;
-        this.collection_id = collection_id;
+        this.fk_artist_id = fk_artist_id;
+        this.fk_collection_id = fk_collection_id;
     }
 }
